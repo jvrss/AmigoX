b3e27b536eddbd906d7ab84ea70e42bd517b5b97
joaosilva <jv_rss@hotmail.com>
2019-09-04 16:05:33 -0300

    Implementações

--- source/app/Http/Controllers/MemberController.php
+++ source/app/Http/Controllers/MemberController.php
@@ -6,34 +6,33 @@ use Illuminate\Http\Request;
 use App\Group;
 use App\User;
 
-class MemberController extends Controller
-{
-    
+class MemberController extends Controller {
+
     function __construct() {
-        
+
         $this->middleware('auth');
-        
     }
-    
+
     public function show($userId) {
-        
+
         $user = User::find($userId);
-        
+
         $group_id = session('group_id');
         $group = Group::find($group_id);
 
         return view('member.show', ['user' => $user, 'group' => $group]);
     }
-    
-    public function edit($id) {
-        session(['group_id' => $id]);
+
+    public function edit($group_id) {
+        session(['group_id' => $group_id]);
+
+        $group = Group::find($group_id);
         
-        $group = Group::find($id);
-        $users = User::all();
-        
-        return view('member.edit', ['group' => $group, 'users' => $users, 'member_id' => $id]);
+        $users = User::where("id", "<>", "");
+
+        return view('member.edit', ['group' => $group, 'users' => $users, 'member_id' => $group_id]);
     }
-    
+
     public function update($id) {
 //        $group = Group::find($id);
 //        $group->update(collect($request->all())->except('_token')->toArray());
